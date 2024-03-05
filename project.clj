09cb843cb2c022edc6f64a7f39809549c686e029
Aleksandr Bogdanov <sasha_bogdanov_dev@yahoo.com>
2024-03-05 23:06:58 +0530

    Group and Remote change

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject weedbreed/lein-template.cloregram "0.2"
+(defproject algoflora/lein-template.cloregram "0.2"
   :description "Template project of Telegram Bot Application using Cloregram framewwork"
   ;:url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
