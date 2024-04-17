f3ffe0268b703492327effc34e8984ffe940e828
Aleksandr Bogdanov <sasha_bogdanov_dev@yahoo.com>
2024-04-17 16:14:00 +0530

    Cloregram 0.10.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.algoflora/lein-template.cloregram "0.3"
+(defproject io.github.algoflora/lein-template.cloregram "0.3.2"
   
   :description "Template project of Telegram Bot Application using Cloregram framewwork"
 
